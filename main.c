ce78bc6fbf9bbf75bfbc69a07e0ada39d161fb17
fadirafik <fadihadddad11@gmail.com>
2023-10-19 19:13:50 +0300

    this is an automated commit

--- main.c
+++ main.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf(stderr, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		printf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(line, sizeof(line), fp))
